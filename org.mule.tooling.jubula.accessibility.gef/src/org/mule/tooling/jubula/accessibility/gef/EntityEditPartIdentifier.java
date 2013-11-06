f10788cd6cd539843d5f5592ff4c8d5a0cec1446
Alejo Abdala <alejo.abdala@mulesource.com>
2013-11-06 14:39:38 -0300

    Modifying the default behavior of entities to add suffixes with indexes if there are more than one of the same class

--- org.mule.tooling.jubula.accessibility.gef/src/org/mule/tooling/jubula/accessibility/gef/EntityEditPartIdentifier.java
+++ org.mule.tooling.jubula.accessibility.gef/src/org/mule/tooling/jubula/accessibility/gef/EntityEditPartIdentifier.java
@@ -1,8 +1,11 @@
 package org.mule.tooling.jubula.accessibility.gef;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.jubula.rc.rcp.e3.gef.identifier.IEditPartIdentifier;
 import org.mule.tooling.messageflow.editpart.EntityEditPart;
 import org.mule.tooling.model.messageflow.MessageFlowEntity;
@@ -39,6 +42,30 @@ public class EntityEditPartIdentifier<T extends EntityEditPart<? extends Message
 	 */
 	@Override
 	public String getIdentifier() {
-		return this.editPart.getClass().getSimpleName();
+		int indexOfPart = 0;
+		String simpleName = editPart.getClass().getSimpleName();
+
+		EditPart parent = editPart.getParent();
+		if (parent != null) {
+			indexOfPart = getIndexOfPartInParent(parent);
+			if (indexOfPart < 0) {
+				throw new RuntimeException("editPart is not a child of it's parent");
+			}
+		}
+
+		String suffix = indexOfPart > 0 ? String.valueOf(indexOfPart) : "";
+		return simpleName + suffix;
+	}
+
+	private int getIndexOfPartInParent(EditPart parent) {
+		List<EditPart> siblings = new ArrayList<EditPart>();
+		@SuppressWarnings("unchecked")
+		List<EditPart> children = parent.getChildren();
+		for (EditPart child : children) {
+			if (child.getClass().equals(editPart.getClass())) {
+				siblings.add(child);
+			}
+		}
+		return siblings.indexOf(editPart);
 	}
 }
